a3a97940f3e8a06adab6b14626ca13e01bbd6a1f
Emerson Hall <e@emersonhall.com>
2025-04-09 13:46:10 -0500

    Updated snapshot to 2025-04-08 (Closes #3)

--- docker/download-snapshot.sh
+++ docker/download-snapshot.sh
@@ -1,4 +1,4 @@
-SNAPSHOT_URL=https://link.storjshare.io/s/jv62jmwhs3n6c7usknlv4ux4lbza/nodedumps/zec/zebra-2024-03-25.tar?download=1
+SNAPSHOT_URL=https://link.storjshare.io/s/jx6hrssshp3rbpdmrxjiybjm3cnq/nodedumps/zec/zebra-2025-04-08.tar?download=1
 
 echo "Downloading blockchain snapshot to speed up sync time..."
 wget -qO- $SNAPSHOT_URL | tar --strip-components=1 -xvf - -C ./data/zebrad-cache
